0db5dc44115d351bbf647db005880206ef865978
Akash <akash@Akashs-MacBook-Air.local>
2018-06-27 11:18:29 +0530

    dd

--- src/common/company.js
+++ src/common/company.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Row, Grid } from "react-bootstrap";
 const Company = () => ({
     render() {
         return (
-
             <div className="companies">
                 <div className="companies-strip" />
             </div>
-
         );
     }
 });
