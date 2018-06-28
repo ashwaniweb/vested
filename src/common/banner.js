a280426fdf41e97bf15effb913bc4b928509a489
Akash <akash@Akashs-MacBook-Air.local>
2018-06-29 01:46:07 +0530

    all

--- src/common/banner.js
+++ src/common/banner.js
@@ -55,7 +55,35 @@ const Banner = ({
                                     alt={imgAlt}
                                 />
                             )}
-                            {/* {img && <img src={img} alt={imgAlt} />} */}
+                            {img && (
+                                <picture>
+                                    <img
+                                        sizes="(max-width: 2880px) 100vw, 2880px"
+                                        srcSet={img}
+                                        srcSet="
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_320.png 320w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_458.png 458w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_570.png 570w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_673.png 673w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_758.png 758w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_844.png 844w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_911.png 911w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_981.png 981w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_1046.png 1046w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_1109.png 1109w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_1170.png 1170w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_1230.png 1230w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_1286.png 1286w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_1342.png 1342w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_1396.png 1396w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_1443.png 1443w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_1498.png 1498w,
+                                        4PhotoGroup_1_rfiyo1_c_scale,w_2880.png 2880w"
+                                        src="4PhotoGroup_1_rfiyo1_c_scale,w_2880.png"
+                                        alt={imgAlt}
+                                    />
+                                </picture>
+                            )}
                         </div>
                     </Row>
                 </Grid>
